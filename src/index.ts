0ed36b179d0b081bb05cb21cc022f7e8fb075c12
Sahandevs <sahandevs@gmail.com>
2019-08-10 11:36:31 +0430

    add models to index export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { createGridViaDataSource } from "./GridGenerator";
 import { createLocalGridGenerator } from "./LocalGridGenerator";
+import { GridResultModel, FilterSchemaType, Schema } from "./GridResultModel";
 
-export { createGridViaDataSource, createLocalGridGenerator };
+export { createGridViaDataSource, createLocalGridGenerator, GridResultModel, FilterSchemaType, Schema };
