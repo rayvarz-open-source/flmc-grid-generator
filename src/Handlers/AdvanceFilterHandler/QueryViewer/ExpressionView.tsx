88b700a10f7c28064fccd4d51a38a3bac203cb00
Sahandevs <sahandevs@gmail.com>
2019-09-01 14:21:46 +0430

    add animation for Expression Delete button

--- src/Handlers/AdvanceFilterHandler/QueryViewer/ExpressionView.tsx
+++ src/Handlers/AdvanceFilterHandler/QueryViewer/ExpressionView.tsx
@@ -1,9 +1,8 @@
-import { Icon, IconButton, Menu, MenuItem, Typography } from "@material-ui/core";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
 import * as React from "react";
-import { Filter, FilterSchemaType, getFilterSchemaTypeName } from "../../../Models/Filter";
+import { Filter, FilterSchemaType } from "../../../Models/Filter";
 import { AndOrExpression } from "./AndOrExpression";
-import { ValueContainerView } from "./ValueContainerView";
+import { FilterExpressionView } from "./FilterExpressionView";
 // const useInputSelectionStyles = makeStyles((theme: Theme) =>
 //   createStyles({
 //     container: {
@@ -37,72 +36,18 @@ import { ValueContainerView } from "./ValueContainerView";
 // }
 //
 //
-const useExpressionStyle = makeStyles((theme: Theme) =>
-  createStyles({
-    container: {
-      backgroundColor: "rgba(0,0,0,0.05)",
-      padding: 3,
-      paddingLeft: 5,
-      paddingRight: 5,
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center"
-    },
-    deleteButton: {
-      padding: 0,
-      transition: "100ms"
-    }
-  })
-);
+
 type ExpressionProps = {
   expression: Filter;
   depth: number;
 };
 export function Expression(props: ExpressionProps) {
-  const classes = useExpressionStyle();
+  
   const { expression, ...others } = props;
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [hover, setHover] = React.useState(false);
-  const filterTypes: FilterSchemaType[] = [FilterSchemaType.EQUAL_BY, FilterSchemaType.LIKE];
-  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
-    setAnchorEl(event.currentTarget);
-  }
-  function handleClose() {
-    setAnchorEl(null);
-  }
+
   if ([FilterSchemaType.AND, FilterSchemaType.OR].includes(expression.type)) {
     return <AndOrExpression depth={props.depth} expression={expression} {...others} />;
   } else {
-    return (
-      <div style={{ display: "flex" }}>
-        <div
-          className={classes.container}
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-          style={{ marginLeft: props.depth == 0 ? 0 : 15 }}
-        >
-          <Typography variant="body2">{expression.fieldName}</Typography>
-          <ValueContainerView value={getFilterSchemaTypeName(expression.type)} onClick={handleClick} />
-          <ValueContainerView value={expression.value || "None"} onClick={handleClick} />
-          <IconButton className={classes.deleteButton} style={{ opacity: hover ? 0.7 : 0.2 }}>
-            <Icon style={{ fontSize: "1rem" }}>{"close"}</Icon>
-          </IconButton>
-
-          <Menu
-            id={`type_select_${expression.fieldName}`}
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            {filterTypes.map((item, i) => (
-              <MenuItem key={`option_${i}`} onClick={handleClose} value={item}>
-                {getFilterSchemaTypeName(item)}
-              </MenuItem>
-            ))}
-          </Menu>
-        </div>
-      </div>
-    );
+    return <FilterExpressionView depth={props.depth} expression={expression} />
   }
 }
---
+++ src/Handlers/AdvanceFilterHandler/QueryViewer/FilterExpressionView.tsx
@@ -0,0 +1,82 @@
+import { Icon, IconButton, Menu, MenuItem, Typography } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import * as React from "react";
+import { Filter, FilterSchemaType, getFilterSchemaTypeName } from "../../../Models/Filter";
+import { ValueContainerView } from "./ValueContainerView";
+
+const useExpressionStyle = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      backgroundColor: "rgba(0,0,0,0.05)",
+      padding: 3,
+      paddingLeft: 5,
+      paddingRight: 5,
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      transition: "300ms"
+    },
+    deleteButton: {
+      padding: 0,
+      transition: "100ms"
+    }
+  })
+);
+
+type Props = {
+  expression: Filter;
+  depth: number;
+};
+
+export function FilterExpressionView(props: Props) {
+  const classes = useExpressionStyle();
+  const { expression } = props;
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const filterTypes: FilterSchemaType[] = [FilterSchemaType.EQUAL_BY, FilterSchemaType.LIKE];
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    setAnchorEl(event.currentTarget);
+  }
+  function handleClose() {
+    setAnchorEl(null);
+  }
+  const [hover, setHover] = React.useState(false);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <div
+        className={classes.container}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        style={{ marginLeft: props.depth == 0 ? 0 : 15 }}
+      >
+        <Typography variant="body2">{expression.fieldName}</Typography>
+        <ValueContainerView value={getFilterSchemaTypeName(expression.type)} onClick={handleClick} />
+        <ValueContainerView value={expression.value || "None"} onClick={handleClick} />
+        <IconButton
+          className={classes.deleteButton}
+          style={{
+            opacity: hover ? 0.7 : 0.0,
+            transitionDelay: hover ? "150ms" : "0ms",
+            width: hover ? "1rem" : "0rem"
+          }}
+        >
+          <Icon style={{ fontSize: "1rem" }}>{"close"}</Icon>
+        </IconButton>
+        <Menu
+          id={`type_select_${expression.fieldName}`}
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          {filterTypes.map((item, i) => (
+            <MenuItem key={`option_${i}`} onClick={handleClose} value={item}>
+              {getFilterSchemaTypeName(item)}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
+    </div>
+  );
+}
