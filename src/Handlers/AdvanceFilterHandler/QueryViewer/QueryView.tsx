f023829a4fa5050d729bdb0a0a3db20c584c6966
Sahandevs <sahandevs@gmail.com>
2019-09-02 16:41:40 +0430

    add actionBar to query view

--- src/Handlers/AdvanceFilterHandler/QueryViewer/QueryView.tsx
+++ src/Handlers/AdvanceFilterHandler/QueryViewer/QueryView.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import * as React from "react";
 import { ExpressionModel } from "./ExpressionModel";
@@ -7,6 +8,21 @@ const useQueryViewStyle = makeStyles((theme: Theme) =>
   createStyles({
     container: {
       flex: 9,
+      flexDirection: "column",
+      display: "flex"
+    },
+    actionBarContainer: {
+      backgroundColor: "rgba(0,0,0,0.05)",
+      width: "100%",
+      display: "flex",
+      flexDirection: "row-reverse",
+      paddingTop: 4,
+      paddingBottom: 4,
+      paddingRight: 10,
+      paddingLeft: 10
+    },
+    queryContainer: {
+      flex: 1,
       display: "flex",
       flexDirection: "row",
       overflow: "auto",
@@ -42,7 +58,17 @@ export function QueryView(props: Props) {
 
   return (
     <div className={classes.container}>
-      <Expression depth={0} expression={props.query} />
+      <div className={classes.queryContainer}>
+        <Expression depth={0} expression={props.query} />
+      </div>
+      <div className={classes.actionBarContainer}>
+        <Button variant={"contained"} color={"primary"}>
+          {"Done"}
+        </Button>
+        <Button variant={"text"} color={"default"} style={{ marginRight: 5, marginLeft: 5 }}>
+          {"Cancel"}
+        </Button>
+      </div>
     </div>
   );
 }
