4022308040ba5db98bd91b34ffea5d9725a2f913
Sahandevs <sahandevs@gmail.com>
2019-09-03 12:41:18 +0430

    remove unused code

--- src/Handlers/AdvanceFilterHandler/QueryViewer/ExpressionView.tsx
+++ src/Handlers/AdvanceFilterHandler/QueryViewer/ExpressionView.tsx
@@ -4,39 +4,6 @@ import { FilterSchemaType } from "../../../Models/Filter";
 import { AndOrExpression } from "./AndOrExpression";
 import { ExpressionModel } from "./ExpressionModel";
 import { FilterExpressionView } from "./FilterExpressionView";
-// const useInputSelectionStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     container: {
-//       width: "1.1rem",
-//       height: "1.1rem",
-//       borderRadius: 5,
-//       transition: "150ms",
-//       backgroundColor: "rgba(0,0,0,0.1)",
-//       marginRight: 3,
-//       marginLeft: 3,
-//       marginBottom: 2
-//     },
-//     icon: {
-//       transition: "150ms"
-//     }
-//   })
-// );
-// type InputSectionProps = {
-//   isSelected: boolean;
-//   onClick: () => void;
-// };
-// function InputSection(props: InputSectionProps) {
-//   const classes = useInputSelectionStyles();
-//   return (
-//     <ButtonBase className={classes.container} onClick={props.onClick} style={{ opacity: props.isSelected ? 0.5 : 0.3 }}>
-//       <Icon className={classes.icon} style={{ opacity: props.isSelected ? 0.5 : 0.3 }}>
-//         {"center_focus_strong"}
-//       </Icon>
-//     </ButtonBase>
-//   );
-// }
-//
-//
 
 type ExpressionProps = {
   expression: ExpressionModel;
